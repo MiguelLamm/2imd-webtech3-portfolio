0bf4998e7ff4151a59e145d80bb879a12e4264b4
Miguel Lammens <justin.bugtong@gmail.com>
2020-03-20 13:42:46 +0100

    testen van functies

--- lab 5/controllers/api/v1/messages.js
+++ lab 5/controllers/api/v1/messages.js
@@ -17,16 +17,27 @@ let getAll = (req, res)=>{
     });
 }
 
-/*let getMessage = (req, res)=>{
+let getMessage = (req, res)=>{
     res.json({
         "status": "succes",
         "data": {
             "messages": {
-                "text" : []
+                "text" : "get message succes"
             }
         }
     });
-}*/
+}
+
+let getMessageId = (req, res)=>{
+    res.json({
+        "status": "succes",
+        "data": {
+            "messages": {
+                "text" : "get message id succes"
+            }
+        }
+    });
+}
 
 let create =(req, res)=>{
     let message = new Message();
@@ -44,11 +55,33 @@ let create =(req, res)=>{
         });
     }
 });
-    //let user = req.body.user;
-    //let message = req.body.message;
-    
+}
+
+let put = (req, res)=>{
+    res.json({
+        "status": "succes",
+        "data": {
+            "messages": {
+                "text" : "put succes"
+            }
+        }
+    });
+}
+
+let del = (req, res)=>{
+    res.json({
+        "status": "succes",
+        "data": {
+            "messages": {
+                "text" : "del succes"
+            }
+        }
+    });
 }
 
 module.exports.getAll=getAll;
-//module.exports.getMessage = getMessage;
+module.exports.getMessage = getMessage;
+module.exports.getMessageId = getMessageId;
 module.exports.create=create;
+module.exports.put=put;
+module.exports.del=del;
--- lab 5/routes/api/v1/messages.js
+++ lab 5/routes/api/v1/messages.js
@@ -4,15 +4,15 @@ const messageController = require('../../../controllers/api/v1/messages');
 
 router.get("/",messageController.getAll);
 
-//router.get("/messages",messageController.getMessage);
+router.get("/messages",messageController.getMessage);
 
-//router.get("/messages/:id",messageController.getMessageId);
+router.get("/messages/:id",messageController.getMessageId);
 
 router.post("/messages/",messageController.create);
 
-//router.put("/messages/:id",messageController.put);
+router.put("/messages/:id",messageController.put);
 
-//router.delete("/messages/:id", messageController.del);
+router.delete("/messages/:id", messageController.del);
 
 //router.get("/messages?user:username",messageController.getMessageUser);
 module.exports = router;
