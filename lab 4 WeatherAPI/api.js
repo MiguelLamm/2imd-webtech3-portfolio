99515f9e1bbed991648730759b8305fdfb455b8f
Miguel Lammens <justin.bugtong@gmail.com>
2020-04-30 13:21:02 +0200

    api aanspreken

--- lab 4 WeatherAPI/api.js
+++ lab 4 WeatherAPI/api.js
@@ -15,8 +15,19 @@ class App{
         this.lat = result.coords.latitude;
         this.lng = result.coords.longitude;
         console.log(this.lat, this.lng);
+        this.getWeather();
     }
 
-   
+   getWeather(){
+    const cors = "https://cors-anywhere.herokuapp.com/";
+       const key= "cf08e49c0e3241eea60ea6a855597072";
+       let url = `${cors}https://api.weatherbit.io/v2.0/current?&lat=${this.lat}&lon=${this.lng}&key=cf08e49c0e3241eea60ea6a855597072`;
+       
+       fetch(url).then(response => {
+           return response.json();
+       }).then(data => {
+           console.log(data);
+       })
+   }
 }
 let app = new App();
